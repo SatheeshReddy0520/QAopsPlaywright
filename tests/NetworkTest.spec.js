6bcadf63313ef25c5cda7af4551cdcd4cdbbcdb8
Satheesh Reddy <satheeshreddy0099@gmail.com>
2026-09-01 14:51:12 +0530

    Fix: Change APIUtils import to default export syntax

--- tests/NetworkTest.spec.js
+++ tests/NetworkTest.spec.js
@@ -1,6 +1,6 @@
 const { test, expect, request } = require('@playwright/test');
 const { json } = require('node:stream/consumers');
-const { APIUtils } = require('../utils/APIUtils');
+const APIUtils = require('../utils/APIUtils');
  const apidata = {userEmail:"satheeshreddy0520@gmail.com",userPassword:"@Reddys143" }
  const placeorderdata = {orders:[{country:"India",productOrderedId:"6960ea76c941646b7a8b3dd5"}] }
  const payloadFakeData={data:[],message:"No Orders"}
@@ -30,27 +30,27 @@ test("WebApi1", async ({ page }) => {
     // 2nd way to wait for element === waitForLoadState("networkidle")
    
 
-     
-     
-      await page.route("https://rahulshettyacademy.com/api/ecom/order/get-orders-for-customer/*",
-       async route=>
-        {
-          const responses= await page.request.fetch(route.request());
-          let data=JSON.stringify(payloadFakeData);
-          route.fulfill({
-            responses,
-            data,
+      
+      
+       await page.route("https://rahulshettyacademy.com/api/ecom/order/get-orders-for-customer/*",
+        async route=>
+         {
+           const responses= await page.request.fetch(route.request());
+           let data=JSON.stringify(payloadFakeData);
+           route.fulfill({
+             responses,
+             data,
+          });
+
          });
-
-        });
-    await page.locator("button[routerlink='/dashboard/myorders']").click();
-    await page.waitForResponse("https://rahulshettyacademy.com/api/ecom/order/get-orders-for-customer/*")
-    console.log(await page.locator(".mt-4").textContent());
+     await page.locator("button[routerlink='/dashboard/myorders']").click();
+     await page.waitForResponse("https://rahulshettyacademy.com/api/ecom/order/get-orders-for-customer/*")
+     console.log(await page.locator(".mt-4").textContent());
 
 
-    }
-    
-    
+     }
+     
+     
 
 
 );
